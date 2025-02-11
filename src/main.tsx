0a513d1d42a90d9f4f4f3cc10f8bfa26ed448261
minhtungo <loducfc@gmail.com>
2025-02-10 20:26:46 -0500

    chore: add tailwindand and integrate shadcn/ui components

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen';
+import { routeTree } from '@/routeTree.gen';
 
 // Set up a Router instance
 const router = createRouter({
