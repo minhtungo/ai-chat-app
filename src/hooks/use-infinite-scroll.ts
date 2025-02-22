fa505027a6a12805a356fe23752851e05d18e3b0
minhtungo <loducfc@gmail.com>
2025-02-21 22:53:09 -0500

    chore

---
+++ src/hooks/use-infinite-scroll.ts
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react';
+
+type UseInfiniteScrollProps = {
+  hasNextPage: boolean;
+  fetchNextPage: () => void;
+  threshold?: number;
+};
+
+export function UseInfiniteScroll({
+  hasNextPage,
+  fetchNextPage,
+  threshold = 0.1,
+}: UseInfiniteScrollProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const target = entries[0];
+        if (target.isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      },
+      {
+        rootMargin: `0px 0px ${threshold}px 0px`,
+      },
+    );
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, [hasNextPage, fetchNextPage, threshold]);
+
+  return { containerRef };
+}
