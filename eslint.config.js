166c98df9013d715aa93c736352fb79ac0d3d6e0
Minh Ngo <mn.minhtungo@gmail.com>
2025-03-17 01:18:32 -0400

    chore: add eslint-plugin-react-compiler and update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,16 @@
 // eslint.config.js
 import pluginRouter from '@tanstack/eslint-plugin-router';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default [
   ...pluginRouter.configs['flat/recommended'],
+  {
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      'react-compiler/react-compiler': 'error',
+    },
+  },
   // Any other config...
 ];
--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,19 @@ import viteReact from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
+// const ReactCompilerConfig = {};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({
       autoCodeSplitting: true,
     }),
-    viteReact(),
+    viteReact({
+      // babel: {
+      //   plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      // },
+    }),
     tailwindcss(),
     viteTsconfigPaths(),
   ],
